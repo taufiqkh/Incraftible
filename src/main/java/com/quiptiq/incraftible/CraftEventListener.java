b48d0af4144c195e6c273a1ee9e504f2159fcae0
Taufiq Hoven <taufiq.hoven@gmail.com>
2012-03-06 13:16:27 +1000

    Updated to reflect Bukkit API changes.

--- src/main/java/com/quiptiq/incraftible/CraftEventListener.java
+++ src/main/java/com/quiptiq/incraftible/CraftEventListener.java
@@ -1,7 +1,16 @@
 package com.quiptiq.incraftible;
 
+import static com.quiptiq.incraftible.message.FixedMessage.LOG_ITEM_CRAFT_ATTEMPT;
+
+import java.util.logging.Logger;
+
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.getspout.spoutapi.event.inventory.InventoryCraftEvent;
 import org.getspout.spoutapi.event.inventory.InventoryListener;
+
+import com.quiptiq.incraftible.message.Message;
 
 /**
  * Simple listener for inventory crafting events.
@@ -9,29 +18,62 @@ import org.getspout.spoutapi.event.inventory.InventoryListener;
  * @author Taufiq Hoven
  */
 public class CraftEventListener extends InventoryListener {
-    /**
-     * Handles any non-cancelled crafting events.
-     */
-    private final CraftEventHandler handler;
+    private final Logger log = Logger.getLogger(Incraftible.DEFAULT_LOGGER);
 
     /**
-     * Create a new listener, using the specified handler to handle intercepted
-     * events.
-     *
-     * @param newHandler
-     *            Handles intercepted events.
+     * Configuration for the behaviour of the crafting checks.
      */
-    public CraftEventListener(CraftEventHandler newHandler) {
-        this.handler = newHandler;
+    private final IncraftibleConfig config;
+
+    /**
+     * Create a new listener for the specified plugin config.
+     *
+     * @param config    Configuration for the plugin behaviour.
+     */
+    public CraftEventListener(IncraftibleConfig config) {
+        this.config = config;
     }
 
+    /*
+     * (non-Javadoc)
+     * Called on attempted crafting within the inventory.
+     *
+     * @see org.getspout.spoutapi.event.inventory.InventoryListener#onInventoryCraft(org.getspout.spoutapi.event.inventory.InventoryCraftEvent)
+     */
     @Override
+    @EventHandler(priority = EventPriority.NORMAL)
     public void onInventoryCraft(InventoryCraftEvent event) {
         if (event.isCancelled() || event.getResult() == null) {
             return;
         }
-        if (!handler.handleCraft(event, event.getResult().getType())) {
+        if (!handleCraft(event, event.getResult().getType())) {
             super.onInventoryCraft(event);
         }
     }
-}
+
+    /**
+     * Handles crafting of an object. If the item is not allowable and the
+     * following option is set to true, the event result is set to null:
+     * <pre>
+     * event.craft.returnvalue.null
+     * </pre>
+     *
+     * @param event
+     *            Crafting event.
+     * @param craftable
+     *            Object created by the crafting.
+     * @return True if the event is handled, false if it was not.
+     */
+    public boolean handleCraft(InventoryCraftEvent event, Material craftable) {
+        if (!config.isItemAllowed(craftable, event.getResult(), event.getPlayer())) {
+            event.getPlayer().sendMessage(Message.PLAYER_MESSAGE_DISALLOWED.prepareMessage(craftable));
+            event.setCancelled(true);
+            // Certain
+            if (config.isEventReturnValueMadeNull()) {
+                event.setResult(null);
+            }
+            log.info(String.format(LOG_ITEM_CRAFT_ATTEMPT, event.getPlayer().getName(), craftable));
+            return true;
+        }
+        return false;
+    }}
--- src/main/java/com/quiptiq/incraftible/Incraftible.java
+++ src/main/java/com/quiptiq/incraftible/Incraftible.java
@@ -1,16 +1,14 @@
 package com.quiptiq.incraftible;
 
-import static com.quiptiq.incraftible.message.FixedMessage.LOG_STANDARD_STRATEGY;
 import static com.quiptiq.incraftible.message.FixedMessage.LOG_DISABLED;
 import static com.quiptiq.incraftible.message.FixedMessage.LOG_ENABLED;
+import static com.quiptiq.incraftible.message.FixedMessage.LOG_STANDARD_STRATEGY;
 import static com.quiptiq.incraftible.message.FixedMessage.LOG_WARN_NO_CONFIG;
 import static com.quiptiq.incraftible.message.FixedMessage.LOG_WARN_NO_SPOUT;
 
 import java.util.List;
 import java.util.logging.Logger;
 
-import org.bukkit.event.Event.Priority;
-import org.bukkit.event.Event.Type;
 import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -68,10 +66,7 @@ public class Incraftible extends JavaPlugin {
             }
             getCommand(FixedMessage.COMMAND_PREFIX).setExecutor(new CommandHandler(config));
 
-            // Leave call to deprecated method as is until new api officially released
-            pluginManager.registerEvent(
-                    Type.CUSTOM_EVENT, new CraftEventListener(new CraftEventHandler(config)), Priority.Normal,
-                    this);
+            pluginManager.registerEvents(new CraftEventListener(config), this);
         }
         log.info(LOG_ENABLED);
     }
